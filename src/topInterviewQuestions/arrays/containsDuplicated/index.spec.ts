a8a815318175d3e35e4d4c425dd79687c013de8f
Johnny Kung <kjohnathan.engi@gmail.com>
2024-09-29 13:21:12 -0700

    Updated solution and test case

--- src/topInterviewQuestions/arrays/containsDuplicated/index.spec.ts
+++ src/topInterviewQuestions/arrays/containsDuplicated/index.spec.ts
@@ -2,8 +2,14 @@ import containsDuplicate from "./index";
 import { describe } from "@jest/globals";
 
 describe('containsDuplicate function', () => {
-  it('should return true when there are at least one duplicate number', () => {
+  it('should return true when there are at least one duplicate number - case 1', () => {
     const sample = [1, 2, 3, 4, 4, 5, 6, 7];
+    const result = containsDuplicate(sample);
+    expect(result).toBe(true);
+  });
+
+  it('should return true when there are at least one duplicate number - case 2', () => {
+    const sample = [0,4,5,0,3,6];
     const result = containsDuplicate(sample);
     expect(result).toBe(true);
   });
