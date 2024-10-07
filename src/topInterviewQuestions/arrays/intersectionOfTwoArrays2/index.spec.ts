361ed621f1f556448f701c88e744418bc248f7a9
Johnny Kung <kjohnathan.engi@gmail.com>
2024-10-06 22:18:28 -0700

    Added with third solutions

--- src/topInterviewQuestions/arrays/intersectionOfTwoArrays2/index.spec.ts
+++ src/topInterviewQuestions/arrays/intersectionOfTwoArrays2/index.spec.ts
@@ -7,5 +7,16 @@ describe('findIntersectionOfTwoArrays', () => {
     expect(findIntersectionOfTwoArrays([], [1, 2, 3])).toEqual([]);
     expect(findIntersectionOfTwoArrays([1, 2, 3], [])).toEqual([]);
     expect(findIntersectionOfTwoArrays([1, 1, 2, 2], [2, 2, 3, 3])).toEqual([2, 2]);
+    const nums1 = [1, 2, 4, 4, 9];
+    const nums2 = [1, 1, 2, 3, 4, 4, 8]
+    const result = findIntersectionOfTwoArrays(nums1, nums2);
+    expect(result).toEqual([1, 2, 4, 4])
+  });
+
+  it('should return the intersection of two arrays - case 2', () => {
+    const nums1 = [1, 2, 2, 1];
+    const nums2 = [2];
+    const result = findIntersectionOfTwoArrays(nums1, nums2);
+    expect(result).toEqual([2])
   });
 });
--- src/topInterviewQuestions/arrays/intersectionOfTwoArrays2/index.ts
+++ src/topInterviewQuestions/arrays/intersectionOfTwoArrays2/index.ts
@@ -10,32 +10,63 @@ function findIntersectionOfTwoArrays(nums1: number[], nums2: number[]): number[]
   // });
   // return result;
 
+
   // Using cache, O(n) and S(n)
-  const cache: {
-    [key: number]: number
-  } = {};
-  nums2.forEach((num) => {
-    if (cache[num] === undefined) {
-      cache[num] = 1;
-    } else {
-      cache[num] += 1;
-    }
-  });
+  // const cache: {
+  //   [key: number]: number
+  // } = {};
+  // nums2.forEach((num) => {
+  //   if (cache[num] === undefined) {
+  //     cache[num] = 1;
+  //   } else {
+  //     cache[num] += 1;
+  //   }
+  // });
 
-  const result: number[] = [];
-  nums1.forEach((num) => {
-    if (cache[num] && cache[num] > 0) {
-      cache[num] -= 1;
-      result.push(num);
-    }
-  });
-  return result;
+  // const result: number[] = [];
+  // nums1.forEach((num) => {
+  //   if (cache[num] && cache[num] > 0) {
+  //     cache[num] -= 1;
+  //     result.push(num);
+  //   }
+  // });
+  // return result;
 
-    // If nums1 and nums2 are sorted
+
+  // If nums1 and nums2 are sorted
   /* 
     For ex, nums1 = [1, 2, 4, 4 9];
     nums2 = [1, 1, 2, 3, 4, 4, 8]
+
+    It's like a table
+       1, 2, 2, 4, 4, 9
+     1 X
+     1 
+     2    X
+     3      
+     4          X
+     4              X
+     8
   */
+ nums1.sort((a, b) => a - b);
+ nums2.sort((a, b) => a - b);
+ console.log('nums1', nums1);
+ const result = [];
+ let cacheValidJ = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = cacheValidJ ; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        result.push(nums1[i]);
+        cacheValidJ = j + 1;
+        break;
+      } else if (nums1[i] > nums2[j]) {
+        continue;
+      } else {
+        break;
+      }
+    }
+  }
+  return result;
 }
 
 export default findIntersectionOfTwoArrays;
