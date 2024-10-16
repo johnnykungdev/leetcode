32fc2ff94457af9ffeeffc75913881d31fa8f094
Johnny Kung <kjohnathan.engi@gmail.com>
2024-10-15 18:51:57 -0700

    Added third solution

--- src/topInterviewQuestions/arrays/plusOne/index.spec.ts
+++ src/topInterviewQuestions/arrays/plusOne/index.spec.ts
@@ -1,29 +1,29 @@
 import plusOne from './index';
 
 describe('plusOne', () => {
-  // test('increments [1,2,3] to [1,2,4]', () => {
-  //   expect(plusOne([1, 2, 3])).toEqual([1, 2, 4]);
-  // });
+  test('increments [1,2,3] to [1,2,4]', () => {
+    expect(plusOne([1, 2, 3])).toEqual([1, 2, 4]);
+  });
 
-  // test('increments [4,3,2,1] to [4,3,2,2]', () => {
-  //   expect(plusOne([4, 3, 2, 1])).toEqual([4, 3, 2, 2]);
-  // });
+  test('increments [4,3,2,1] to [4,3,2,2]', () => {
+    expect(plusOne([4, 3, 2, 1])).toEqual([4, 3, 2, 2]);
+  });
 
-  // test('increments [9] to [1,0]', () => {
-  //   expect(plusOne([9])).toEqual([1, 0]);
-  // });
+  test('increments [9] to [1,0]', () => {
+    expect(plusOne([9])).toEqual([1, 0]);
+  });
 
-  // test('increments [1,0,0] to [1,0,1]', () => {
-  //   expect(plusOne([1, 0, 0])).toEqual([1, 0, 1]);
-  // });
+  test('increments [1,0,0] to [1,0,1]', () => {
+    expect(plusOne([1, 0, 0])).toEqual([1, 0, 1]);
+  });
 
-  // test('increments [0] to [1]', () => {
-  //   expect(plusOne([0])).toEqual([1]);
-  // });
+  test('increments [0] to [1]', () => {
+    expect(plusOne([0])).toEqual([1]);
+  });
 
-  // test('increments [9,9,9] to [1,0,0,0]', () => {
-  //   expect(plusOne([9, 9, 9])).toEqual([1, 0, 0, 0]);
-  // });
+  test('increments [9,9,9] to [1,0,0,0]', () => {
+    expect(plusOne([9, 9, 9])).toEqual([1, 0, 0, 0]);
+  });
 
   test('increments [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3] to [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]', () => {
     expect(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3])).toEqual([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4]);
--- src/topInterviewQuestions/arrays/plusOne/index.ts
+++ src/topInterviewQuestions/arrays/plusOne/index.ts
@@ -29,32 +29,58 @@ function plusOne(digits: number[]): number[] {
   // return numsArray;
 
   // second solution
-  const result = [];
-  let shouldCarry = false;
-  let firstDigit = digits[digits.length - 1];
-  let firstDigitAfter = firstDigit + 1;
-  if (firstDigitAfter === 10) {
-    result.push(0);
-    shouldCarry = true;
-  } else {
-    result.push(firstDigitAfter);
-  }
+  // let result: number[] = [];
+  // let shouldCarry = false;
+  // let firstDigit = digits[digits.length - 1];
+  // let firstDigitAfter = firstDigit + 1;
+  // if (firstDigitAfter === 10) {
+  //   result.push(0);
+  //   shouldCarry = true;
+  // } else {
+  //   result.push(firstDigitAfter);
+  // }
 
-  for (let i = digits.length - 2; i >= -1; i--) {
-    const num = digits[i];
-    if (shouldCarry && num === undefined) {
-      result.push(1);
-      shouldCarry = false;
-    } else if (shouldCarry && num + 1 === 10) {
-      result.push(0);
-    } else if (shouldCarry) {
-      result.push(num + 1);
-      shouldCarry = false;
-    } else if (num !== undefined) {
-      result.push(num);
+  // if (shouldCarry) {
+  //   for (let i = digits.length - 2; i >= -1; i--) {
+  //     const num = digits[i];
+  //     if (shouldCarry && num === undefined) {
+  //       result.push(1);
+  //       shouldCarry = false;
+  //     } else if (shouldCarry && num + 1 === 10) {
+  //       result.push(0);
+  //     } else if (shouldCarry) {
+  //       result.push(num + 1);
+  //       shouldCarry = false;
+  //     } else if (num !== undefined) {
+  //       result.push(num);
+  //     }
+  //   }
+  // } else {
+  //   const copiedDigits = [...digits];
+  //   result = [...copiedDigits.slice(0, -1), firstDigitAfter];
+  //   return result;
+  // }
+
+  // return result.reverse();
+
+  //third solution
+  //recursive
+  const addOne = (index: number): number[] => {
+    // Base case: if we go past the first digit, we need to add a new digit
+    if (index < 0) return digits = [1, ...digits];
+    
+    // If the current digit is less than 9, we can simply increment it
+    if (digits[index] < 9) {
+        digits[index]++;
+        return digits; // Return the updated digits
     }
-  }
-  return result.reverse();
+    
+    // If the current digit is 9, set it to 0 and carry over
+    digits[index] = 0; 
+    return addOne(index - 1); // Recur for the next digit
+  };
+
+  return addOne(digits.length - 1);
 }
 
 export default plusOne;
