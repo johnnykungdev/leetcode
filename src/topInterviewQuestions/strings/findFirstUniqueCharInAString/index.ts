b91fa2a84f0717ead4a8b18724cf9be6ae368137
Johnny Kung <kjohnathan.engi@gmail.com>
2024-12-01 02:01:21 -0800

    Added second solution

--- src/topInterviewQuestions/strings/findFirstUniqueCharInAString/index.ts
+++ src/topInterviewQuestions/strings/findFirstUniqueCharInAString/index.ts
@@ -33,22 +33,38 @@ function firstUniqChar(s: string): number {
   // first solution
   // Time complexity: O(n)
   // Space Complexity: O(n)
-  const stringArr = s.split('');
-  let cachedMap: {
-    [key: string]: number
-  } = {};
-  for (let i = 0; i < stringArr.length; i++) {
-    const char = stringArr[i];
-    if (cachedMap[char] === undefined) {
-      cachedMap[char] = 1;
-    } else {
-      cachedMap[char] += 1;
-    }
+  // const stringArr = s.split('');
+  // let cachedMap: {
+  //   [key: string]: number
+  // } = {};
+  // for (let i = 0; i < stringArr.length; i++) {
+  //   const char = stringArr[i];
+  //   if (cachedMap[char] === undefined) {
+  //     cachedMap[char] = 1;
+  //   } else {
+  //     cachedMap[char] += 1;
+  //   }
+  // }
+
+  // for (let i = 0; i < stringArr.length; i++) {
+  //   const currentChar = stringArr[i];
+  //   if (cachedMap[currentChar] === 1) {
+  //     return i;
+  //   }
+  // }
+  // return -1;
+
+  // second solution
+  // Use an fixed array to fill in
+  const fixedArray: number[] = new Array(26).fill(0);
+  for (let char of s) {
+    const charOrder = char.charCodeAt(0) - 'a'.charCodeAt(0);
+    fixedArray[charOrder] += 1;
   }
 
-  for (let i = 0; i < stringArr.length; i++) {
-    const currentChar = stringArr[i];
-    if (cachedMap[currentChar] === 1) {
+  for (let i = 0; i < s.length; i++) {
+    const currentCharOrder = s.charCodeAt(i) - 'a'.charCodeAt(0);
+    if (fixedArray[currentCharOrder] === 1) {
       return i;
     }
   }
